52218d7ac4c6399e530924df88999c73d1cb0ae3
UXDev25 <uxdev25@gmail.com>
2026-05-11 22:40:44 +0200

    feat: added Game developer page
    closes #2

---
+++ VideoGameManagerEF/Pages/Developers/Details.cshtml.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using VideoGameManagerEF.Data;
+
+namespace VideoGameManager.Pages.Developers;
+
+public class DetailsDev : PageModel
+{
+    private readonly GameStoreContext _context;
+    public DetailsDev(GameStoreContext context) => _context = context;
+    
+    public void OnGet(int id)
+    {
+        
+    }
+}
